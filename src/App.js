cb4ac6baacbace04cc87bd785be24a4da7e2d738
shanid <shanid77shan@gmail.com>
2026-04-14 13:45:43 +0530

    update tunnel url

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ export default function App() {
     }, 1800);
 
     try {
-      const res = await axios.post("https://seven-chefs-poke.loca.lt/signal", {
+      const res = await axios.post("https://clean-snails-share.loca.lt/signal", {
         symbol: selected.symbol,
         label:  selected.label,
       });
